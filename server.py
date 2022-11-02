c5214d56b6f86b054e3296f270ff86f9452d40da
xiaoguoyu <xiao_guoyu@126.com>
2022-11-02 17:55:39 +0800

    v1.1

--- server.py
+++ server.py
@@ -1,18 +1,1 @@
-import asyncio
-import websockets
-
-
-async def sendback(websocket):
-    with open("data", "r") as data:
-        wall = data.read()
-        await websocket.send(wall)
-        print("A client connected.")
-    name = await websocket.recv()
-    with open("data", "a") as data:
-        data.write("\n\n————————————————\n\n"+name)
-        print("A client send something.")
-
-start_server = websockets.serve(sendback, "0.0.0.0", 35888)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# 核心代码从1.1版本开始不再开源
